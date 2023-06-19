f0335f248b22571d9d963b510d2faec0fbbb3d51
Fuku-hiro112 <134264092+Fuku-hiro112@users.noreply.github.com>
2023-06-19 16:23:55 +0900

    横移動、スピードゲージの追加
    ・自動的に横移動するようになりました
    ・MAXスピードは６です
    ・速度によってゲージが変わります

---
+++ Assets/Sqript/Fukuda/GameDirector.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    Image speedGage;
+    PlayerAction playerAction;
+    float speed;
+
+    void Start()
+    {
+        speedGage = GameObject.Find("SpeedGage").GetComponent<Image>();
+        playerAction = GameObject.Find("Player").GetComponent<PlayerAction>();
+        //playerAction = GameObject.Find("Player").GetComponent<PlayerAction>();
+    }
+
+    
+    void Update()
+    {
+        speed = playerAction.speed_elapsed_time;
+        //Debug.Log(speed);
+        speedGage.fillAmount = Speed_rate();
+    }
+
+    float Speed_rate()
+    {
+        float rate = 0;
+        rate = speed/6;
+        Debug.Log(rate);
+        return rate;
+    }
+}
--- Assets/Sqript/Fukuda/PlayerAction.cs
+++ Assets/Sqript/Fukuda/PlayerAction.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed_elapsed_time = 0;
+    [SerializeField] float speed = 5;
+    bool canMove;
+
+    Rigidbody2D rb;
+
+    //public float Speed_elapTime{get; set;}
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+
+
+        canMove = true;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
+        if (canMove)
+        {
+            Move();
+        }
+        else
+        {
+            
+        }
+    }
+
+    void Move()
+    {
+        if (speed_elapsed_time > 6)
+        {
+            speed_elapsed_time = 6;
+        }
+        else
+        {
+            speed_elapsed_time += Time.deltaTime;
+        }
+
+        rb.velocity = new Vector3(speed * speed_elapsed_time, rb.velocity.y);
+        //Debug.Log(rb.velocity);
     }
 }
