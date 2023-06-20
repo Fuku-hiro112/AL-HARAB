988a12a2e2adc0081da7505df9dfd8654171461c
Fuku-hiro112 <134264092+Fuku-hiro112@users.noreply.github.com>
2023-06-20 23:59:38 +0900

    下の線路から上の線路に移動できるようになった
    ・エンター、左クリックでジャンプし、velorsity<=0だった場合enabled=trueになる
    ・水のシェーダーグラフにした

--- Assets/Sqript/Fukuda/PlayerAction.cs
+++ Assets/Sqript/Fukuda/PlayerAction.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerAction : MonoBehaviour
 {
     public float speed_elapsed_time = 0;
     [SerializeField] float speed = 5;
-    float jumpSpeed = 7.5f;
-    bool canMove;
-    private bool isGround = false;
+    [SerializeField] float jump_speed = 7.5f;
+    [SerializeField] GameObject upper_line_tile;
+
+    TilemapCollider2D upper_line_collider;
+    
+    private bool is_ground = false;
+    private bool can_move;
+    private bool can_change_line;
+    private bool can_enable;
     private GroundCheck ground;
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
 
     //public float Speed_elapTime{get; set;}
 
@@ -18,29 +25,37 @@ public class PlayerAction : MonoBehaviour
     {
         ground = GameObject.Find("GroundCheck").GetComponent<GroundCheck>();
         rb = GetComponent<Rigidbody2D>();
+        upper_line_collider = upper_line_tile.GetComponent<TilemapCollider2D>();
 
-        canMove = true;
+        can_move = true;
+        can_change_line = false;
+        can_enable = false;
     }
 
 
     void Update()
     {
-        isGround = ground.IsGround();
+        is_ground = ground.IsGround();
 
-        if (canMove)
+        if (can_move)
         {
             Move();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        if (Input.GetKeyDown(KeyCode.Space) && is_ground)
         {
             Jamp();
         }
 
         if (( Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0) ) 
-            && isGround)
+            && is_ground)
         {
+            can_change_line = true;
+        }
 
+        if (can_change_line)
+        {
+            Change_Line();
         }
     }
 
@@ -63,10 +78,23 @@ public class PlayerAction : MonoBehaviour
 
     void Jamp()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        rb.velocity = new Vector2(rb.velocity.x, jump_speed);
     }
-    void Trolley_Switching()
+    void Change_Line()
     {
+        float pos_y = transform.position.y;
+        can_enable = true;
 
+        if (pos_y < -2)
+        {
+            Jamp();
+            can_enable = true;
+        }
+
+        if (can_enable && rb.velocity.y <= 0)
+        {
+            upper_line_collider.enabled = true;
+            can_change_line = false;
+        }
     }
 }
