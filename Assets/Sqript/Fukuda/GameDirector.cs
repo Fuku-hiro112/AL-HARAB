b74b78baec8566f72ad2b1947d8d77a5de0ad31b
Fuku-hiro112 <134264092+Fuku-hiro112@users.noreply.github.com>
2023-07-17 10:56:08 +0900

    GameOver時にResolt画面に移行し、結果を表示するようにしました

--- Assets/Sqript/Fukuda/GameDirector.cs
+++ Assets/Sqript/Fukuda/GameDirector.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using System;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,36 +8,48 @@ using UnityEngine.UI;
 
 public class GameDirector : MonoBehaviour
 {
+    [SerializeField] private float _waitTime;
     private Image _speedGage;
     private Text _timeText;
-    private PlayerAction _playerAction;
+    private PlayerAction _playerAct;
+    //PlayerAction.GameMode _gameMode;
     private float _speed;
     private string _sceneName;
 
     private GameObject[] _tagEnemys;
     private GameObject[] _tagBarricades;
 
-    private byte _enemyNum;
-    private byte _barricadeNum;
+    [NonSerialized]
+    public static int _enemyNum;
+    [NonSerialized]
+    public static byte _barricadeNum;
 
-    public byte _breakableObjNum;
+    [NonSerialized]
+    public static byte _breakableObjNum;
+    [NonSerialized]
+    public static float _clearTime;
 
     void Start()
     {
         FindConponent();
 
+        //óvÇÁÇ»Ç¢Ç©Ç‡
         _sceneName = SceneManager.GetActiveScene().name;
-
+        
         _tagEnemys = GameObject.FindGameObjectsWithTag("enemy");
         _tagBarricades = GameObject.FindGameObjectsWithTag("barricade");
-        _enemyNum = (byte)_tagEnemys.Length;
+
+        _enemyNum = _tagEnemys.Length;
         _barricadeNum = (byte)_tagBarricades.Length;
         _breakableObjNum = Convert.ToByte(_enemyNum + _barricadeNum);
+
+        PlayerPrefs.SetInt("EnemyNum",_enemyNum);
+        PlayerPrefs.SetInt("BarricadeNum",_barricadeNum);
     }
     
-    void Update()
+    async void Update()
     {
-        if (_playerAction == null)
+        if (_playerAct == null)
         {
             Debug.Log("nullÇ≈Ç∑");
         }
@@ -44,39 +58,73 @@ public class GameDirector : MonoBehaviour
             _speedGage.fillAmount = SpeedRate();
 
             //ì_êîÇï\é¶
-            _timeText.text = PlayerAction.ClearTime.ToString("F2");
+            _timeText.text = _playerAct.ClearTime.ToString("F2");
         }
+
+        if (_playerAct._mode == PlayerAction.GameMode.Play) return;
+        if (_playerAct._mode == PlayerAction.GameMode.GameCrear)
+        {
+            PlayerPrefs.SetFloat("ClearTime",_playerAct.ClearTime);
+            PlayerPrefs.SetInt("HpCurrent",_playerAct.HpCurrent);
+            SaveBreakableObj();
+        }
+        else if(_playerAct._mode == PlayerAction.GameMode.GameOver)
+        {
+            SaveBreakableObj();
+        }
+        PlayerPrefs.Save();
+        var ct = this.GetCancellationTokenOnDestroy();
+        await AsyncLoadScene(ct,_waitTime);
     }
 
-    
-
-    float SpeedRate()
+    void SaveBreakableObj()
     {
-        _speed = _playerAction.SpeedGage;
+        PlayerPrefs.SetInt("BreakEnemyNum", _playerAct.BreakEnemyNum);
+        PlayerPrefs.SetInt("BreakBarricadeNum", _playerAct.BreakBarricadeNum);
+    }
+
+    private float SpeedRate()
+    {
+        _speed = _playerAct.SpeedGage;
         return _speed / 6;
     }
 
-    void FindConponent()
+    private void FindConponent()
     {
         _timeText = GameObject.Find("TimeText").GetComponent<Text>();
         _speedGage = GameObject.Find("SpeedGage").GetComponent<Image>();
-        _playerAction = GameObject.Find("Player").GetComponent<PlayerAction>();
+        _playerAct = GameObject.Find("Player").GetComponent<PlayerAction>();
     }
 
-    private void GameEnd()
+    private void GameEnd(float clearTime)
     {
+
         switch (_sceneName)
         {
             case "GameScene1":
 
                 break;
             case "GameScene2":
+
                 break;
             case "GameScene3":
+
                 break;
             default:
                 
                 break;
         }
+    }
+    private void GameOver()
+    {
+
+    }
+
+    private async UniTask AsyncLoadScene(CancellationToken ct,float waitTime)
+    {
+
+        await UniTask.Delay((int)(waitTime * 1000), cancellationToken: ct);
+
+        SceneManager.LoadScene("ResultScene");
     }
 }
---
+++ Assets/Sqript/Fukuda/ResoltManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResoltManager : MonoBehaviour
+{
+    [SerializeField] private Text _txtNumberOfBroken;
+    [SerializeField] private Text _txtClearTime;
+    [SerializeField] private Text _txtClearScore;
+    [SerializeField] private Text _txtRank;
+
+    private float _clearTime;
+    private int _hpCurrent;
+
+    private int _enemyNum;
+    private int _barricadeNum;
+    
+    private int _breakEnemyNum;
+    private int _breakBarricadeNum;
+
+    private float _totalScore;
+    private string _rank = "F";
+
+    private const int _noneClear = -1; 
+
+    private const int _scoreS = 9000;
+    private const int _scoreA = 8000;
+    private const int _scoreB = 6000;
+
+    void Start()
+    {
+        _clearTime = PlayerPrefs.GetFloat("ClearTime", _noneClear);
+        _hpCurrent = PlayerPrefs.GetInt("HpCurrent", 0);
+        _enemyNum = PlayerPrefs.GetInt("EnemyNum", 0);
+        _barricadeNum = PlayerPrefs.GetInt("BarricadeNum", 0);
+        _breakEnemyNum = PlayerPrefs.GetInt("BreakEnemyNum", 0);
+        _breakBarricadeNum = PlayerPrefs.GetInt("BreakBarricadeNum", 0);
+
+        _totalScore = ScoreCalculation();
+
+        _txtNumberOfBroken.text = $"{_breakEnemyNum}/{_enemyNum}";
+
+        if (_clearTime == _noneClear)
+        {
+            _rank = "F";
+            _txtRank.text = _rank;
+            _txtClearScore.text = $"{100 * _breakEnemyNum + 100 * _breakBarricadeNum}";
+            _txtClearTime.text = "~~~";
+            return;
+        }
+
+        if (_totalScore >= _scoreS) _rank = "S";
+        else if (_totalScore >= _scoreA) _rank = "A";
+        else if (_totalScore >= _scoreB) _rank = "B";
+        else _rank = "C";
+        _txtRank.text = _rank;
+        _txtClearTime.text = $"{_clearTime.ToString("F")} SEC";
+        _txtClearScore.text = _totalScore.ToString("d4");
+    }
+    float ScoreCalculation()
+    {
+        float timeScone = 10000 - (this._clearTime * 115);
+        float enemyScore = 100 * _breakEnemyNum + 100 * _breakBarricadeNum;
+        float hpScore = _hpCurrent * 1000;
+        return timeScone + enemyScore + hpScore;
+    }
+    void Update()
+    {
+        
+    }
+}
