5d7f78722bb19e174ca211ff9d99eee2240c35bf
masatohasegawa20040413 <135950885+masatohasegawa20040413@users.noreply.github.com>
2023-07-17 15:26:45 +0900

    ステージ選択画面を更新しました。

--- Assets/Sqript/Masato/LoadeScene.cs
+++ Assets/Sqript/Masato/LoadeScene.cs
@@ -5,20 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+   
+    
     public void Stage1()
     {
+
         SceneManager.LoadScene("GameScene1");
+
     }
     public void Stage2()
     {
+
         SceneManager.LoadScene("GameScene2");
+
     }
     public void Stage3()
     {
+
         SceneManager.LoadScene("GameScene3");
+
     }
     public void Tutorial()
     {
+
         SceneManager.LoadScene("TutorialScene");
+
     }
 }
